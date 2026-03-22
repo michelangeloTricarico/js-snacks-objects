0e26b3fabf5d889dd8bd514f99357c4e10adfb4f
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-22 14:17:03 +0100

    snack 1 list initialization

---
+++ snack1/script.js
@@ -0,0 +1,32 @@
+// Esercizio: Fizz Buzz
+
+/* Traccia: 
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore. */
+
+// Array creation
+const bikes = [
+    {
+        name: "Giant",
+        weight: 22
+    },
+    {
+        name: "Cannondale",
+        weight: 20
+    },
+    {
+        name: "Specialized",
+        weight: 21
+    },
+    {
+        name: "Canyon",
+        weight: 25
+    },
+    {
+        name: "Bianchi",
+        weight: 23
+    }
+]
+
+console.log(bikes) //print list

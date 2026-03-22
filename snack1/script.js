0fe43f08e7e2263cb7593fd57506a19c19de9d3b
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-22 14:35:09 +0100

    snack 1 finalization

--- snack1/script.js
+++ snack1/script.js
@@ -30,3 +30,24 @@ const bikes = [
 ]
 
 console.log(bikes) //print list
+
+// main code
+let name_low 
+let weight_low
+result = get_low_weight(bikes)
+console.log(result[0],result[1]) // print function results
+
+// function definition to find bike with minimum weight
+function get_low_weight(bikes){
+    let min_value = 0
+    let bike_model
+    for (let i = 0; i<bikes.length; i++){
+        let actual_obj = bikes[i] 
+        console.log(actual_obj)
+        if (actual_obj["weight"] < min_value || min_value == 0){
+            min_value = actual_obj["weight"] 
+            bike_model = actual_obj["name"] 
+        }
+    }
+    return [bike_model,min_value]
+}
